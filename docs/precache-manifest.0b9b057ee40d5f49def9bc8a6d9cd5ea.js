9db1da95765045c7e387ee17bf484adf945abef9
Banga <prateekbanga12@gmail.com>
2018-12-28 04:11:06 +0530

    added docs folder for github

---
+++ docs/precache-manifest.0b9b057ee40d5f49def9bc8a6d9cd5ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab8204c21ea8df7a3c50",
+    "url": "/static/js/main.ab8204c2.chunk.js"
+  },
+  {
+    "revision": "7c9b36d93b2b5fbd91c9",
+    "url": "/static/js/1.7c9b36d9.chunk.js"
+  },
+  {
+    "revision": "ab8204c21ea8df7a3c50",
+    "url": "/static/css/main.5ffb8139.chunk.css"
+  },
+  {
+    "revision": "1519689084e1bd60a66036192db12e33",
+    "url": "/index.html"
+  }
+];
