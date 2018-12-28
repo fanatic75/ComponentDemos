9832a440144474445470f87c792e0c8efe8aeeb7
Banga <prateekbanga12@gmail.com>
2018-12-28 15:54:17 +0530

    added files for github pages

---
+++ precache-manifest.959fc7732f706e395e739860c515b423.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "45a0fe1bc41afa171c56",
+    "url": "/static/js/main.45a0fe1b.chunk.js"
+  },
+  {
+    "revision": "7c9b36d93b2b5fbd91c9",
+    "url": "/static/js/1.7c9b36d9.chunk.js"
+  },
+  {
+    "revision": "45a0fe1bc41afa171c56",
+    "url": "/static/css/main.5ffb8139.chunk.css"
+  },
+  {
+    "revision": "22dd4022d30c259ea0bed71415b796d2",
+    "url": "/index.html"
+  }
+];
