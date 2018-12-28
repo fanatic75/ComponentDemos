37e224727018f0223ea944ef4d4e3cd9682ab799
Banga <prateekbanga12@gmail.com>
2018-12-28 16:08:38 +0530

    added deploy folder

---
+++ deploy/precache-manifest.26991bbf7684fc0178f927516f0d87c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f8ccb7ebd906cdc57cd",
+    "url": "/static/js/main.1f8ccb7e.chunk.js"
+  },
+  {
+    "revision": "7c9b36d93b2b5fbd91c9",
+    "url": "/static/js/1.7c9b36d9.chunk.js"
+  },
+  {
+    "revision": "1f8ccb7ebd906cdc57cd",
+    "url": "/static/css/main.5ffb8139.chunk.css"
+  },
+  {
+    "revision": "73f6fd40fb43f32b727d36f2729cf24c",
+    "url": "/index.html"
+  }
+];
